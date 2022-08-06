8df9e11b877f2035a99f869e24a3a9f227f644bd
Raul - Anpad <raul_sigoli@hotmail.com>
2022-08-06 20:28:47 -0300

    React Router DOM
    Nessa aula, veremos sobre como configurar a navegação da nossa aplicação para que ela tenha múltiplas páginas.
    
    Para isso, vamos instalar a biblioteca React Router DOM e iremos estruturar a navegação da nossa aplicação.

--- src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonContainer, ButtonVariant } from './Button.styles'
-
-interface ButtonPorps {
-  variant?: ButtonVariant
-}
-
-export function Button({ variant = 'primary' }: ButtonPorps) {
-  return <ButtonContainer variant={variant}>Enviar</ButtonContainer>
-}
